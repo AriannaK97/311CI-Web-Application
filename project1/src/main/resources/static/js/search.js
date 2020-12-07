e44fe5d9906cffb0345ac0e722f286de45dadb34
Anna <ankatkav@gmail.com>
2020-12-07 02:43:27 +0200

    locked fields in update ui - fixed table in search ui

--- project1/src/main/resources/static/js/search.js
+++ project1/src/main/resources/static/js/search.js
@@ -18,6 +18,45 @@ $( document ).ready(function() {
         return false;
     });
 
+/*    if($("#searcher.searchType").val() === "1"){
+        let queryType = "Requests per type that were created within a specified time range"
+    }else if($("#searcher.searchType").val() === "2"){
+        let queryType = "Τotal requests per day for a specific request type and time range"
+    }else if($("#searcher.searchType").val() === "3"){
+        let queryType = "Μost common service request per zipcode for a specific date"
+    }else if($("#searcher.searchType").val() === "4"){
+        let queryType = "Average completion time per service request for a specific date range"
+    }else if($("#searcher.searchType").val() === "5"){
+        let queryType = "Most common service request in a specified bounding box for a specific day"
+    }else if($("#searcher.searchType").val() === "6"){
+        let queryType = "Top-5 Special Service Areas (SSA) with regards to total number of requests per day\n" +
+            "                                    for a specific date range"
+    }else if($("#searcher.searchType").val() === "7"){
+        let queryType = "License plates involved in abandoned vehicle complaints more\n" +
+            "                                    than once"
+    }else if($("#searcher.searchType").val() === "8"){
+        let queryType = "Second most common color of vehicles involved in abandoned vehicle complaints"
+    }else if($("#searcher.searchType").val() === "9"){
+        let queryType = "Rodent baiting requests where the number of premises baited is less than a specified\n" +
+            "                                    number"
+    }else if($("#searcher.searchType").val() === "10"){
+        let queryType = "Rodent baiting requests where the number of premises with garbage is less than a specified\n" +
+            "                                    number"
+    }else if($("#searcher.searchType").val() === "11"){
+        let queryType = "Rodent baiting requests where the number of premises with rats is less than a specified\n" +
+            "                                    number"
+    }else if($("#searcher.searchType").val() === "12"){
+        let queryType = "Police districts that have handled “pot holes” requests with more than one number\n" +
+            "                                    of potholes on the same day that they also handled “rodent baiting” requests with more than\n" +
+            "                                    one number of premises baited, for a specific day"
+    }else if($("#searcher.searchType").val() === "13"){
+        let queryType = "Find Incidents by zip code"
+    }else if($("#searcher.searchType").val() === "14"){
+        let queryType = "Find Incidents by street address"
+    }else if($("#searcher.searchType").val() === "15"){
+        let queryType = "Find incidents by street address and zip code"
+    }*/
+
 
     if (parseInt($("#page").val()) > 0)
         $(".previous").show();
